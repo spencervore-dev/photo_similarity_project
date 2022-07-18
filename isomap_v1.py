ef2666711dd55bc4537396d200553c0cdca8c720
mkaffash3.gatech.edu <98723090+mojtabat67@users.noreply.github.com>
2022-07-18 17:51:31 -0400

    isomap_v1 minor adjustment

--- isomap_v1.py
+++ isomap_v1.py
@@ -32,7 +32,7 @@ for count, filename in enumerate(glob.glob(filepath)):
 #         print(images[count].shape)
 
 num_images = len(images)
-image_dimensions = (350, 525, 3)
+image_s = (350, 525, 3)
 
 #########################################################
 ###### flatten data and convert it to data frame ########
@@ -47,7 +47,7 @@ df = pd.DataFrame(lst)
 df.head()
 
 #####################################################
-######## ISOMAP with 6 neighbors and reduce to 2 dimensions (each image dimensions reduces to 2)
+######## ISOMAP with 6 neighbors and reduce to 2 s (each image s reduces to 2)
 #####################################################
 
 iso = manifold.Isomap(n_neighbors=6, n_components=2)
@@ -55,7 +55,7 @@ iso.fit(lst)
 manifold_2Da = iso.transform(lst)
 manifold_2D = pd.DataFrame(manifold_2Da, columns=['Component 1', 'Component 2'])
 
-# Left with 2 dimensions
+# Left with 2 s
 manifold_2D.head()
 
 ####################################
@@ -63,22 +63,22 @@ manifold_2D.head()
 ####################################
 
 fig = plt.figure()
-fig.set_size_inches(10, 10)
+fig.set_size_inches(9, 9)
 ax = fig.add_subplot(111)
-ax.set_title('2D Components from Isomap of Animal Images')
+ax.set_title('2D Components from Isomap')
 ax.set_xlabel('Component: 1')
 ax.set_ylabel('Component: 2')
 
 # plot Isomap results
-x_size = (max(manifold_2D['Component 1']) - min(manifold_2D['Component 1'])) * 0.08
-y_size = (max(manifold_2D['Component 2']) - min(manifold_2D['Component 2'])) * 0.08
+x_size = (max(manifold_2D['Component 1']) - min(manifold_2D['Component 1'])) * 0.12
+y_size = (max(manifold_2D['Component 2']) - min(manifold_2D['Component 2'])) * 0.12
 for i in range(40):
     img_num = np.random.randint(0, num_images)
     x0 = manifold_2D.loc[img_num, 'Component 1'] - (x_size / 2.)
     y0 = manifold_2D.loc[img_num, 'Component 2'] - (y_size / 2.)
     x1 = manifold_2D.loc[img_num, 'Component 1'] + (x_size / 2.)
     y1 = manifold_2D.loc[img_num, 'Component 2'] + (y_size / 2.)
-    img = lst[img_num].reshape(image_dimensions)
+    img = lst[img_num].reshape(image_s)
     ax.imshow(img, aspect='auto', cmap=plt.cm.gray, 
               interpolation='nearest', zorder=100000, extent=(x0, x1, y0, y1))
 
