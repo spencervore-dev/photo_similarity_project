ac0271b6bbbc42db4e642406549770c22b3804eb
Spencer Vore <69659970+spencervore-dev@users.noreply.github.com>
2022-07-22 23:01:15 -0600

    Attemp to make isomap more memory efficient. Updated some documentation for the repo as well.

--- README.md
+++ README.md
@@ -1,15 +1,26 @@
-# PEXEL DOWNLOAD SCRIPT DOCUMENTATION
+# Image similarity project
+The goal of this project is to take a collection of images and try to map them into a space where we can find other 
+visually similar images.
 
-This script will download all the available images on pexel matching search criteria using the pexel api.
-
+## Project Sturcture
+1) Pexels download stuff: pexels_api_pull.py, config_template.py - use these scripts to download up to 8000 images from
+   www.pexels.com to get a dataset.
+2) Pixabay download stuff: pixabay_api_pull.py, config_pixabay_template.py - These scripts are very similar to the Pexels
+    scripts, except they were modified to run on Pixabay to try an alternate data source. Pixabay's API is limited to
+    about 600 photos per search.
+3) isomap: isomap_v1.py: To map the images into a space where the nearest neighbors are visually similar, we tried the
+    isomap algorithm in this script.
 
 ## Environment
 This script was developed in Python 3.10.4. Install requirements.txt for 3rd party packages:
 ```pip install -r requirements.txt```
 
 
-## Usage
-Copy the file named config_template.py into config.py, add your secret API key, and adjust the settings. 
+## PEXEL DOWNLOAD SCRIPT DOCUMENTATION
+
+This script will download all the available images on pexel matching search criteria using the pexel api.
+
+To use, copy the file named config_template.py into config.py, add your secret API key, and adjust the settings. 
 Ensure that config.py is not added to your git repo by running ```git status``` after you make the config.py
 file so no secret keys get added to our repo.
 
@@ -20,5 +31,13 @@ Then, run the file pexel_api_pull.py to start the download based on the settings
 matching your API query will be saved into whichever directory you defined as your download location.
 
 
+### PIXABAY DOWNLOAD SCRIPT DOCUMENTATION
+Very similar to the design of teh pexels script. Copy the file named config_pixabay_template.py to config_pixabay.py
+and add your Pixabay API key, which you can get by setting up an account on pixabay.com.
+
+Pixabay API documentation is here:
+https://pixabay.com/api/docs/
+
+Run the file pixabay_api_pull.py to start pulling data from pixabay based on the settings in your config_pixabay.py file.
 
 
--- isomap_v1.py
+++ isomap_v1.py
@@ -30,11 +30,14 @@ import numpy as np
 # read a series of images in a folder and save them in a list
 #############################################################
 
-filepath = "/Users/mojtabataghipourkaffash/Desktop/Courses/ISYE 6740/Final Project/GitHub/images/*.jpeg"
+## Configuration
+filepath = "/home/spencervore/OneDrive/isye6740_project_data/pexel/test_mojtaba/*.jpeg"
+make_plots = True
 
 #################################################################
 # Find minimum dimension of the images (x or y dim) in the folder
 #################################################################
+print("Finding min dimensions")
 dimensions = []
 for filename in glob.glob(filepath):
     dimensions.append(iio.imread(filename).shape[0])
@@ -44,6 +47,7 @@ min_dimension = min(dimensions)
 #################################################################
 # zero padding to make the images square
 #################################################################
+print("Zero padding and standardizing image sizes")
 images = []
 for filename in glob.glob(filepath):
     rgb = iio.imread(filename)
@@ -74,27 +78,35 @@ for filename in glob.glob(filepath):
     else:
         rgb = rgb[int(lower_cut):int(upper_cut), int(lower_cut):int(upper_cut), ]
 
-    images.append(rgb)
+    images.append(rgb.flatten())
 
+images_count = len(images)
+
+'''
 #################################################################
 # flatten data and convert it to data frame
 #################################################################    
 
+print("Flattening image list")
 lst = []
-for i in range(len(images)):
-    lst.append(images[i].flatten())
-    
+for i in range(images_count, 0, -1):
+    # Loop backwards and delete each item as we come to it to keep down data size.
+    lst.append(images.pop(i).flatten())
+
+del images
+'''
+
 pd.options.display.max_columns = 7
-df = pd.DataFrame(lst)
+df = pd.DataFrame(images)
 df.head()
 
 #####################################################
 ######## ISOMAP with 6 neighbors and reduce to 2 s (each image s reduces to 2)
 #####################################################
-
+print("Run Isomap")
 iso = manifold.Isomap(n_neighbors=6, n_components=2)
-iso.fit(lst)
-manifold_2Da = iso.transform(lst)
+iso.fit(images)
+manifold_2Da = iso.transform(images)
 manifold_2D = pd.DataFrame(manifold_2Da, columns=['dimension 1', 'dimension 2'])
 
 # Left with 2 dimensions
@@ -103,39 +115,46 @@ manifold_2D.head()
 ####################################
 ########### Plotting ###############
 ####################################
+print("Plotting results")
+if make_plots:
+    fig = plt.figure()
+    fig.set_size_inches(9, 9)
+    ax = fig.add_subplot(111)
+    ax.set_title('Fnial 2D from Isomap of Animal Images')
+    ax.set_xlabel('dimension: 1')
+    ax.set_ylabel('dimension: 2')
 
-fig = plt.figure()
-fig.set_size_inches(9, 9)
-ax = fig.add_subplot(111)
-ax.set_title('Fnial 2D from Isomap of Animal Images')
-ax.set_xlabel('dimension: 1')
-ax.set_ylabel('dimension: 2')
+    # plot Isomap results
+    num_images = :qimages_count
+    image_dimensions = (min_dimension, min_dimension, 3)
+    x_size = (max(manifold_2D['dimension 1']) - min(manifold_2D['dimension 1'])) * 0.12
+    y_size = (max(manifold_2D['dimension 2']) - min(manifold_2D['dimension 2'])) * 0.12
+    # for i in range(40):
+    #     img_num = np.random.randint(0, num_images)
+    #     x0 = manifold_2D.loc[img_num, 'dimension 1'] - (x_size / 2.)
+    #     y0 = manifold_2D.loc[img_num, 'dimension 2'] - (y_size / 2.)
+    #     x1 = manifold_2D.loc[img_num, 'dimension 1'] + (x_size / 2.)
+    #     y1 = manifold_2D.loc[img_num, 'dimension 2'] + (y_size / 2.)
+    #     img = lst[img_num].reshape(image_dimensions)
+    #     ax.imshow(img, aspect='auto', cmap=plt.cm.gray, 
+    #               interpolation='nearest', zorder=100000, extent=(x0, x1, y0, y1))
 
-# plot Isomap results
-num_images = len(images)
-image_dimensions = (min_dimension, min_dimension, 3)
-x_size = (max(manifold_2D['dimension 1']) - min(manifold_2D['dimension 1'])) * 0.12
-y_size = (max(manifold_2D['dimension 2']) - min(manifold_2D['dimension 2'])) * 0.12
-# for i in range(40):
-#     img_num = np.random.randint(0, num_images)
-#     x0 = manifold_2D.loc[img_num, 'dimension 1'] - (x_size / 2.)
-#     y0 = manifold_2D.loc[img_num, 'dimension 2'] - (y_size / 2.)
-#     x1 = manifold_2D.loc[img_num, 'dimension 1'] + (x_size / 2.)
-#     y1 = manifold_2D.loc[img_num, 'dimension 2'] + (y_size / 2.)
-#     img = lst[img_num].reshape(image_dimensions)
-#     ax.imshow(img, aspect='auto', cmap=plt.cm.gray, 
-#               interpolation='nearest', zorder=100000, extent=(x0, x1, y0, y1))
+    for img_num in range(len(images)):
+        x0 = manifold_2D.loc[img_num, 'dimension 1'] - (x_size / 2.)
+        y0 = manifold_2D.loc[img_num, 'dimension 2'] - (y_size / 2.)
+        x1 = manifold_2D.loc[img_num, 'dimension 1'] + (x_size / 2.)
+        y1 = manifold_2D.loc[img_num, 'dimension 2'] + (y_size / 2.)
+        img = images[img_num].reshape(image_dimensions)
+        ax.imshow(img, aspect='auto', cmap=plt.cm.gray, 
+                  interpolation='nearest', zorder=100000, extent=(x0, x1, y0, y1))
 
-for img_num in range(len(lst)):
-    x0 = manifold_2D.loc[img_num, 'dimension 1'] - (x_size / 2.)
-    y0 = manifold_2D.loc[img_num, 'dimension 2'] - (y_size / 2.)
-    x1 = manifold_2D.loc[img_num, 'dimension 1'] + (x_size / 2.)
-    y1 = manifold_2D.loc[img_num, 'dimension 2'] + (y_size / 2.)
-    img = lst[img_num].reshape(image_dimensions)
-    ax.imshow(img, aspect='auto', cmap=plt.cm.gray, 
-              interpolation='nearest', zorder=100000, extent=(x0, x1, y0, y1))
+    # Show 2D components plot
+    ax.scatter(manifold_2D['dimension 1'], manifold_2D['dimension 2'], marker='.',alpha=0.7)
 
-# Show 2D components plot
-ax.scatter(manifold_2D['dimension 1'], manifold_2D['dimension 2'], marker='.',alpha=0.7)
+    plt.show()
 
-plt.show()
+############################################
+#### Find closest image in vector space ####
+############################################
+# Maybe pick a few sample images, and then display / indicate which is the closest image
+
