0b0a068095e5cefcb425ce71fe4a19eb0eb266bf
Spencer Vore <69659970+spencervore-dev@users.noreply.github.com>
2022-07-30 13:40:00 -0600

    experimenting with isomap

--- isomap_v1.py
+++ isomap_v1.py
@@ -5,19 +5,17 @@ Created on Mon Jul 18 03:46:56 2022
 
 @author: mojtabataghipourkaffash
 """
-from sklearn.datasets import load_digits
-from sklearn.manifold import Isomap
+
 from sklearn import manifold
-from matplotlib import pyplot as plt
-import matplotlib.image as mpimg
 import imageio as iio
-import math
 import pandas as pd
-import scipy.io
 import glob
 
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
+import os
+
 
 #############################################################
 # read a series of images in a folder and save them in a list
@@ -26,18 +24,28 @@ import numpy as np
 ## Configuration
 # filepath = "/home/spencervore/OneDrive/isye6740_project_data/pexel/green_medium/*.jpeg"
 filepath = "/home/spencervore/OneDrive/isye6740_project_data/pexel/test_mojtaba/*.jpeg"
-filepath = "/home/spencervore/OneDrive/isye6740_project_data/pixabay/green/*.jpg"
+filepath = "/home/spencervore/OneDrive/isye6740_project_data/pixabay/transparent/"  # *.png"
 
 make_plots = True
 
+
+'''
 #################################################################
 # Find minimum dimension of the images (x or y dim) in the folder
 #################################################################
 print("Finding min dimensions")
 dimensions = []
+n_layers = 0
 for filename in glob.glob(filepath):
-    dimensions.append(iio.imread(filename).shape[0])
-    dimensions.append(iio.imread(filename).shape[1])
+    filedata = iio.v3.imread(filename)
+    dimensions.append(filedata.shape[0])
+    dimensions.append(filedata.shape[1])
+    if n_layers == 0:
+        n_layers = filedata.shape[2]
+    else:
+        # Check that all images have same number of layers
+        assert n_layers == filedata.shape[2]
+
 min_dimension = min(dimensions)
 
 #################################################################
@@ -46,7 +54,7 @@ min_dimension = min(dimensions)
 print("Zero padding and standardizing image sizes")
 images = []
 for filename in glob.glob(filepath):
-    rgb = iio.imread(filename)
+    rgb = iio.v3.imread(filename)
     diff = max(rgb.shape[0:2]) - min(rgb.shape[0:2]) # find the number of zero pad
     zpad = int(diff/2)+1
     x, y = rgb.shape[1], rgb.shape[0]
@@ -73,40 +81,31 @@ for filename in glob.glob(filepath):
     lower_cut = (rgb_shape - min_dimension)/2
     upper_cut = rgb_shape - (rgb_shape - min_dimension)/2
 
-    '''
-    if (lower_cut % 2) == 0:
-        rgb = rgb[int(lower_cut):int(upper_cut)+1, int(lower_cut):int(upper_cut)+1, ]
-    else:
-        rgb = rgb[int(lower_cut):int(upper_cut), int(lower_cut):int(upper_cut), ]
-    '''
-    # I think the above was making this come out as two different sizes. For this to work, everything must be exactly
-    # the same size no matter how it's processed.
     rgb = rgb[int(lower_cut):int(upper_cut), int(lower_cut):int(upper_cut), ]
 
     images.append(rgb.flatten())
 
-images=np.array(images)
-images_count = len(images)
-
-'''
-#################################################################
-# flatten data and convert it to data frame
-#################################################################    
-
-print("Flattening image list")
-lst = []
-for i in range(images_count, 0, -1):
-    # Loop backwards and delete each item as we come to it to keep down data size.
-    lst.append(images.pop(i).flatten())
-
-del images
+images = np.array(images)
 '''
 
-'''
-pd.options.display.max_columns = 7
-df = pd.DataFrame(images)
-df.head()
-'''
+########## Initialize ##########
+# read in image, flatten it, put it into an array
+r = 500
+c = 500
+flat_images = []
+pixels = []
+for img in os.listdir(filepath):
+    pic = cv2.imread(os.path.join(filepath, img))
+    #pic = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
+    pic = cv2.resize(pic, (r, c))
+    # flatten image
+    pic = np.array(pic.reshape(r * c, 3))
+    flat_img = pic.flatten()
+
+    flat_images.append(flat_img)
+
+images = np.array(flat_images)
+
 
 
 #####################################################
@@ -115,50 +114,41 @@ df.head()
 print("Run Isomap")
 iso = manifold.Isomap(n_neighbors=6, n_components=2)
 iso.fit(images)
-manifold_2Da = iso.transform(images)
-manifold_2D = pd.DataFrame(manifold_2Da, columns=['dimension 1', 'dimension 2'])
+manifold_raw = iso.transform(images)
+manifold = pd.DataFrame(manifold_raw)
 
 # Left with 2 dimensions
-manifold_2D.head()
+manifold.head()
 
 ####################################
 ########### Plotting ###############
 ####################################
-print("Plotting results")
+print("Plotting results of top 2 components")
 if make_plots:
     fig = plt.figure()
     fig.set_size_inches(9, 9)
     ax = fig.add_subplot(111)
-    ax.set_title('Fnial 2D from Isomap of Animal Images')
-    ax.set_xlabel('dimension: 1')
-    ax.set_ylabel('dimension: 2')
+    ax.set_title('Final 2D from Isomap of Animal Images')
+    ax.set_xlabel('dimension: 0')
+    ax.set_ylabel('dimension: 1')
 
     # plot Isomap results
-    num_images = images_count
-    image_dimensions = (min_dimension, min_dimension, 3)
-    x_size = (max(manifold_2D['dimension 1']) - min(manifold_2D['dimension 1'])) * 0.12
-    y_size = (max(manifold_2D['dimension 2']) - min(manifold_2D['dimension 2'])) * 0.12
-    # for i in range(40):
-    #     img_num = np.random.randint(0, num_images)
-    #     x0 = manifold_2D.loc[img_num, 'dimension 1'] - (x_size / 2.)
-    #     y0 = manifold_2D.loc[img_num, 'dimension 2'] - (y_size / 2.)
-    #     x1 = manifold_2D.loc[img_num, 'dimension 1'] + (x_size / 2.)
-    #     y1 = manifold_2D.loc[img_num, 'dimension 2'] + (y_size / 2.)
-    #     img = lst[img_num].reshape(image_dimensions)
-    #     ax.imshow(img, aspect='auto', cmap=plt.cm.gray, 
-    #               interpolation='nearest', zorder=100000, extent=(x0, x1, y0, y1))
+    image_dimensions = (min_dimension, min_dimension, n_layers)
+    x_size = (max(manifold[0]) - min(manifold[0])) * 0.12
+    y_size = (max(manifold[1]) - min(manifold[1])) * 0.12
 
     for img_num in range(len(images)):
-        x0 = manifold_2D.loc[img_num, 'dimension 1'] - (x_size / 2.)
-        y0 = manifold_2D.loc[img_num, 'dimension 2'] - (y_size / 2.)
-        x1 = manifold_2D.loc[img_num, 'dimension 1'] + (x_size / 2.)
-        y1 = manifold_2D.loc[img_num, 'dimension 2'] + (y_size / 2.)
+        x0 = manifold.loc[img_num, 0] - (x_size / 2.)
+        y0 = manifold.loc[img_num, 1] - (y_size / 2.)
+        x1 = manifold.loc[img_num, 0] + (x_size / 2.)
+        y1 = manifold.loc[img_num, 1] + (y_size / 2.)
         img = images[img_num].reshape(image_dimensions)
+
         ax.imshow(img, aspect='auto', cmap=plt.cm.gray, 
                   interpolation='nearest', zorder=100000, extent=(x0, x1, y0, y1))
 
     # Show 2D components plot
-    ax.scatter(manifold_2D['dimension 1'], manifold_2D['dimension 2'], marker='.',alpha=0.7)
+    ax.scatter(manifold[0], manifold[1], marker='.', alpha=0.7)
 
     plt.show()
 
