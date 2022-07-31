2e91078365512ea964beed3a4d461c54fb922235
leseanbrown <65139775+leseanbrown@users.noreply.github.com>
2022-07-31 16:04:18 -0600

    Latest version of PCA with K-means clustering

--- PCA_RGB.py
+++ PCA_RGB.py
@@ -7,28 +7,17 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-from img_recommender import img_recommender
-
 #############################################################
 # read a series of images in a folder and save them in a list
 #############################################################
 
 ## Configuration
-# path = r'C:\Users\lesea\Georgia Institute of Technology\Vore, Spencer E - isye6740_project_data\pixabay\test_transparent'
+## transparent
+path = r'C:\Users\lesea\Georgia Institute of Technology\Vore, Spencer E - isye6740_project_data\pixabay\test_transparent'
 
-# path = "/home/spencervore/OneDrive/isye6740_project_data/pixabay/transparent"
-# images_to_rec = [0, 1, 2, 3, 100, 101, 102, 103, 104, 105, 106, 107, 150, 151, 152, 153, 154, 155, 156, 61, 11, 413, 380]
-# save_loc = "/home/spencervore/OneDrive/isye6740_project_data/results/image_recs/pixabay_transparent_pca_n2/"
-# rec_save_loc = save_loc + "pixa-transparent-iso-"
-# n_components = 2
+## background
+#path = r'C:\Users\lesea\Georgia Institute of Technology\Vore, Spencer E - isye6740_project_data\test'
 
-path = "/home/spencervore/OneDrive/isye6740_project_data/pixabay/transparent"
-images_to_rec = [0, 1, 2, 3, 100, 101, 102, 103, 104, 105, 106, 107, 150, 151, 152, 153, 154, 155, 156, 61, 11, 413, 380]
-save_loc = "/home/spencervore/OneDrive/isye6740_project_data/results/image_recs/pixabay_transparent_pca_n6/"
-rec_save_loc = save_loc + "pixa-transparent-iso-"
-n_components = 6
-
-run_pca = False
 ### for some reason this one image does not work:
 # "pixabay_image__pg0002_ind125_imgid7086605_animals__animal_feline_tiger"
 
@@ -53,7 +42,7 @@ flat_pics = np.array(flat_pics)
 ######## PCA with 2 PCs
 #####################################################
 print("Running PCA")
-pc = n_components
+pc = 2
 pca = PCA(n_components=pc)
 pca_imgs = pca.fit_transform(flat_pics)
 
@@ -61,7 +50,26 @@ x = np.array(pca_imgs[:, 0])
 y = np.array(pca_imgs[:, 1])
 
 #####################################################
-########### Outliers ###############
+########### Create Scree Plot ###############
+# #####################################################
+print("Creating Scree Plot")
+pc_values = np.arange(pca.n_components_) + 1
+plt.plot(pc_values, pca.explained_variance_ratio_, 'ro-', linewidth=2)
+plt.title('Scree Plot')
+plt.xlabel('Principal Component')
+plt.ylabel('Proportion of Variance Explained')
+
+for xs, ys in zip(pc_values, pca.explained_variance_ratio_):
+    label = "{:.2f}".format(ys)
+    plt.annotate(label, (xs, ys),
+                 textcoords='offset points',
+                 xytext=(0, 5))
+
+#plt.savefig('C:/Users/lesea/OneDrive/Documents/GA Tech/Summer 2022/ISYE 6740/Project/pca_scree_plot_background.png')
+plt.show()
+
+#####################################################
+########### Outliers (if needed) ###############
 #####################################################
 print("Removing Outliers")
 # find outliers
@@ -81,100 +89,97 @@ y = np.array(pca_imgs[:, 1])
 #####################################################
 ########### Plotting ###############
 # #####################################################
-print("Plotting results")
+if pc == 2:
+    print("Plotting results")
 
-fig, ax = plt.subplots()
-plt.title('2D Plot Using PCA of RGB Animal Images')
-plt.xlabel('Principal Component 1')
-plt.ylabel('Principal Component 2')
+    fig, ax = plt.subplots()
+    plt.title('2D Plot Using PCA of RGB Animal Images with Transparent Backgrounds')
+    plt.xlabel('Principal Component 1')
+    plt.ylabel('Principal Component 2')
 
-x_size = (max(x) - min(x))
-y_size = (max(y) - min(y))
-size = min(x_size, y_size)
-x_size = size * .075
-y_size = size * .075
+    x_size = (max(x) - min(x))
+    y_size = (max(y) - min(y))
+    size = min(x_size, y_size)
+    x_size = size * .075
+    y_size = size * .075
 
-pic = 0
-for p in pca_imgs:
-    x0 = p[0] - (x_size / 2)
-    x1 = p[0] + (x_size / 2)
-    y0 = p[1] - (y_size / 2)
-    y1 = p[1] + (y_size / 2)
+    pic = 0
+    for p in pca_imgs:
+        x0 = p[0] - (x_size / 2)
+        x1 = p[0] + (x_size / 2)
+        y0 = p[1] - (y_size / 2)
+        y1 = p[1] + (y_size / 2)
 
-    img = flat_pics[pic].reshape(r, c, 3)
+        img = flat_pics[pic].reshape(r, c, 3)
 
-    ax.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), extent=(x0, x1, y0, y1))
-    ax.scatter(x, y, marker="None", alpha=0.7)
+        ax.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), extent=(x0, x1, y0, y1))
+        ax.scatter(x, y, marker="None", alpha=0.7)
 
-    pic += 1
+        pic += 1
 
-plt.savefig(save_loc + 'pca_RGB.png')
-# plt.show()
+    plt.savefig('C:/Users/lesea/OneDrive/Documents/GA Tech/Summer 2022/ISYE 6740/Project/pca_RGB_transparent.png')
+    plt.show()
 
 
 ###########################################
 ##### Clustering #####
 ###########################################
-if run_pca:
-    print("Running K-means clustering")
-    n = 10
-    kmeans = KMeans(n_clusters=n, random_state=0).fit(pca_imgs)
-    labels = kmeans.labels_
+print("Running K-means clustering")
+n = 10
+kmeans = KMeans(n_clusters=n, random_state=0).fit(pca_imgs)
+labels = kmeans.labels_
 
-    # assign pictures to respective label
-    i = 0
-    zero = []
-    one = []
-    two = []
-    three = []
-    four = []
-    five = []
-    six = []
-    seven = []
-    eight = []
-    nine = []
-    for l in labels:
-        if l == 0:
-            zero.append(flat_pics[i])
-        elif l == 1:
-            one.append(flat_pics[i])
-        elif l == 2:
-            two.append(flat_pics[i])
-        elif l == 3:
-            three.append(flat_pics[i])
-        elif l == 4:
-            four.append(flat_pics[i])
-        elif l == 5:
-            five.append(flat_pics[i])
-        elif l == 6:
-            six.append(flat_pics[i])
-        elif l == 7:
-            seven.append(flat_pics[i])
-        elif l == 8:
-            eight.append(flat_pics[i])
-        elif l == 9:
-            nine.append(flat_pics[i])
+# assign pictures to respective label
+i = 0
+zero = []
+one = []
+two = []
+three = []
+four = []
+five = []
+six = []
+seven = []
+eight = []
+nine = []
+for l in labels:
+    if l == 0:
+        zero.append(flat_pics[i])
+    elif l == 1:
+        one.append(flat_pics[i])
+    elif l == 2:
+        two.append(flat_pics[i])
+    elif l == 3:
+        three.append(flat_pics[i])
+    elif l == 4:
+        four.append(flat_pics[i])
+    elif l == 5:
+        five.append(flat_pics[i])
+    elif l == 6:
+        six.append(flat_pics[i])
+    elif l == 7:
+        seven.append(flat_pics[i])
+    elif l == 8:
+        eight.append(flat_pics[i])
+    elif l == 9:
+        nine.append(flat_pics[i])
+    i += 1
+
+clusters = [zero, one, two, three, four, five, six, seven, eight, nine]
+
+# show images in a given cluster
+count = 1
+proj_folder = "C:/Users/lesea/OneDrive/Documents/GA Tech/Summer 2022/ISYE 6740/Project/PCA6_Clusters_background/"
+for clust in clusters:
+    fig = plt.figure(figsize=(10, 10), tight_layout=True)
+    name = str('Cluster ' + str(count) + ' animals (with background) - PC = ' + str(pc))
+    i = 1
+    for im in clust:
+        cols = 6
+        rows = math.ceil(np.shape(clust)[0] / cols)
+        fig.add_subplot(rows, cols, i)
+        plt.imshow(cv2.cvtColor(im.reshape(r, c, 3), cv2.COLOR_BGR2RGB))
+        plt.axis('off')
         i += 1
-
-    clusters = [zero, one, two, three, four, five, six, seven, eight, nine]
-
-    # show images in a given cluster
-    count = 1
-    proj_folder = "C:/Users/lesea/OneDrive/Documents/GA Tech/Summer 2022/ISYE 6740/Project/PCA6_Clusters/"
-    for clust in clusters:
-        fig = plt.figure(figsize=(10, 10), tight_layout=True)
-        name = str('Cluster ' + str(count) + ' animals - PC = ' + str(pc))
-        #plt.title(title, fontsize=12)
-        i = 1
-        for im in clust:
-            cols = 6
-            rows = math.ceil(np.shape(clust)[0] / cols)
-            fig.add_subplot(rows, cols, i)
-            plt.imshow(cv2.cvtColor(im.reshape(r, c, 3), cv2.COLOR_BGR2RGB))
-            plt.axis('off')
-            i += 1
-        plt.savefig(proj_folder + name)
-        plt.show()
-        count += 1
-
-img_recommender(pca_imgs, flat_pics, images_to_rec, rec_save_loc, n_components=n_components, r=r, c=c)
+    plt.savefig(proj_folder + name)
+    plt.show()
+    count += 1
